81bbc9e9f331670bc2757ca94010371cbb7c0de0
oguzhan2027 <oguzhancelik2027@gmail.com>
2023-08-01 15:53:15 +0300

    Add project files.

---
+++ Jwt/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using Jwt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+       
+        [HttpGet("[Action]")]
+        public IActionResult Login()
+        {
+            return Created("",new BuildToken().CreateToken());
+        }
+        [Authorize]
+        [HttpGet("[Action]")]
+        public IActionResult CustomerList()
+        {
+            return Ok("müşteri listesine eriştiniz");
+        }
+    }
+}
